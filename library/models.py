a607f5d9fe8e039e786d82828c799481406f5788
samariddin7444 <samariddinbaxtiyorov05@gmail.com>
2024-04-03 09:56:51 +0500

    sss

--- library/models.py
+++ library/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-
+class Library(models.Model):
+    firth_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    age = models.PositiveIntegerField()
--- student/models.py
+++ student/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
+class Auther(models.Model):
+    firth_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.firth_name} {self.last_name}"
+
+
 class Student(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
     price = models.FileField()
     count = models.IntegerField(default=1)
+    auther = models.ForeignKey(auther, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
